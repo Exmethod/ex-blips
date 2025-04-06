265c8f3677fa54c61bb637b44fe8b388ad48ca94
Ex_method <115433269+Exmethod@users.noreply.github.com>
2025-04-06 11:40:01 -0400

    Update blips.lua

--- CustomBlips/blips/blips.lua
+++ CustomBlips/blips/blips.lua
@@ -5,12 +5,11 @@
 -- ║╚══╦╬╬╣║║║║═╣╚╣║║║╚╝║╚╝║
 -- ╚═══╩╝╚╩╩╩╩══╩═╩╝╚╩══╩══╝
 -- Add as much stuff as you need
--- Cfx Blips website: https://docs.fivem.net/docs/game-references/blips/
--- Make Sure all of the Blips have spaces whe it say example= 1,
+-- Cfx Blips website: https://docs.fivem.net/docs/game-references/blips/\
 local blips = {
 
-     {title="Example", size=1.0, colour=52, id=141, category= example, x = 1737.1256, y = 3027.9854, z = 67.9953},
-     {title="Example2", size=1.0, colour=51, id=142, category= example, x = 1757.1256, y = 3017.9854, z = 65.9953}
+     {title="Example", size=1.0, colour=52, id=141, x = 1737.1256, y = 3027.9854, z = 67.9953},
+     {title="Example2", size=1.0, colour=51, id=142, x = 1757.1256, y = 3017.9854, z = 65.9953}
 }
 
 -- Do not touch any thing under this
@@ -21,7 +20,6 @@ Citizen.CreateThread(function()
       SetBlipSprite(info.blip, info.id)
       SetBlipDisplay(info.blip, 2)
       SetBlipScale(info.blip, info.size)
-      SetBlipCategory(info.blip, info.category)
       SetBlipColour(info.blip, info.colour)
       SetBlipAsShortRange(info.blip, true)
       BeginTextCommandSetBlipName("STRING")
